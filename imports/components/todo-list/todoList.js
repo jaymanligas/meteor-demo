8d53d6d8e83ba27e57bc58675fca6d0388f62767
Jay T Manligas <jaymanligas@gmail.com>
2016-09-29 17:56:25 +0800

    3 way data binding

--- imports/components/todo-list/todoList.js
+++ imports/components/todo-list/todoList.js
@@ -4,21 +4,69 @@ import template from './todoList.html';
 import { Tasks } from '../../api/tasks.js';
 
 class TodosListCtrl {
-  constructor($scope) {
-   $scope.viewModel(this);
 
-   this.helpers({
-     tasks() {
-       return Tasks.find({});
-     }
-   })
- }
+  constructor($scope) {
+
+    $scope.viewModel(this);
+
+    this.helpers({
+      
+      tasks() {
+        const selector = {};
+        // If hide completed is checked, filter tasks
+
+        if (this.getReactively('hideCompleted')) {
+          console.log('reactive')
+          selector.checked = {
+            $ne: true
+          };
+        }
+        // Show newest tasks at the top
+        return Tasks.find(selector, { sort: {  createdAt: -1  } })
+      }
+    })
+
+  }
+
+  addTask(newTask) {
+    // Insert a task into the collection
+    Tasks.insert({
+      text: newTask,
+      createdAt: new Date,
+      checked : false
+    });
+    // Clear form
+    this.newTask = '';
+  }
+
+  setChecked(task) {
+    // Set the checked property to the opposite of its current value
+    Tasks.update(task._id, {
+      $set: {
+        checked: !task.checked
+      },
+    });
+  }
+
+  removeTask(task) {
+    Tasks.remove(task._id);
+  }
+
+  incompleteCount() {
+    return Tasks.find({checked: false}).count();
+  }
 }
+
+
+
+
+
+
 
 export default angular.module('todosList', [
   angularMeteor
 ])
-  .component('todosList', {
-    templateUrl: 'imports/components/todo-list/todoList.html',
-    controller: ['$scope', TodosListCtrl]
-  });
+.component('todosList', {
+  templateUrl: 'imports/components/todo-list/todoList.html',
+  controller: ['$scope', TodosListCtrl]
+});
